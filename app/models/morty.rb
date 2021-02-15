5846ce658c91debf348425dd8dcce83cf92d9e3e
Bryan Nakache <nakachebryan@gmail.com>
2021-02-15 17:14:27 +0100

    Morty Model creation

---
+++ app/models/morty.rb
@@ -0,0 +1,19 @@
+class Morty < ApplicationRecord
+
+   RARITY = ["common", "rare", "epic"]
+   STATE = ["hurt","relaxed","depressed","angry"]
+
+  belongs_to :user
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :photo, presence: true
+  validates :description, presence: true
+
+  validates :state, inclusion: { in: STATE,
+      message: "%{value} is not a valid state type" }, presence: true
+
+  validates :rarity, inclusion: { in: RARITY,
+      message: "%{value} is not a valid rarity type" }, presence: true
+
+
+end
