ed2c77295d277f40f1c04e92f20d030e2752702d
Bryan Nakache <nakachebryan@gmail.com>
2021-02-16 12:23:18 +0100

    test model et controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :morties, only: [:new,:create,:show] do
     resources :reservations, only: :create
+     resources :reviews, only: [:new, :create]
   end
   resources :reservations, only: :show
+   resources :reviews, only: [:edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
