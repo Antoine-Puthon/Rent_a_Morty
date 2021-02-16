c68795af74736e9ce3acaaadc4fa7c30b282e70f
Bryan Nakache <nakachebryan@gmail.com>
2021-02-16 10:49:53 +0100

    fixbug home page

---
+++ app/controllers/morties_controller.rb
@@ -0,0 +1,26 @@
+class MortiesController < ApplicationController
+
+  def new
+    @morty = Morty.new
+  end
+
+  def create
+    @morty = Morty.new(morty_params)
+    @morty.user = current_user
+    if @morty.save
+      redirect_to morty_path(@morty)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @morty = Morty.find(params[:id])
+  end
+
+  private
+
+  def morty_params
+    params.require(:morty).permit(:name,:price,:description,:state,:rarity,:photo)
+  end
+end
