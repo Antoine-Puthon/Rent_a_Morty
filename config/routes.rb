93de6370e09933dcae4a2f8509ed085098859da1
maxime lacaussague <maximelacaus@gmail.com>
2021-02-18 12:55:31 +0100

    creating namespace route and view for my morties

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
   end
 
   resources :reservations, only: [:show, :destroy]
+  namespace :my do
+    resources :morties, only: :index
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
